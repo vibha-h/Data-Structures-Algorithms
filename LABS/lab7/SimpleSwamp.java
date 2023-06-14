cf280be584e6b05b6898e2c2be06e9534b5dd4b7
Vibha Hodachalli <95110043+vibha-h@users.noreply.github.com>
2023-06-14 11:03:42 -0400

    Add files via upload

---
+++ LABS/lab7/SimpleSwamp.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class SimpleSwamp
+{
+	public static void main(String[] args) throws Exception
+	{	
+		int x= (15*2 - 15/4)%7;
+		System.out.println(x);
+	}
+}
